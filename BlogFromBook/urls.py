62312f3985b3042378e45c3ce011942f0ae72575
wjanaszek <trash.wjanaszek@gmail.com>
2017-07-11 18:23:07 +0200

    Added sitemap

--- BlogFromBook/urls.py
+++ BlogFromBook/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from MyBlg.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^MyBlg/', include('MyBlg.urls',
                            namespace='MyBlg',
                            app_name='MyBlg')),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
